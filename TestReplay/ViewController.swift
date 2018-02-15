487505f64e9d96e5b441e70b19f234da74d0ffa5
Eric <winstoncire@gmail.com>
2018-02-15 14:56:17 -0200

    to com medo

--- TestReplay/ViewController.swift
+++ TestReplay/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import ReplayKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, RPScreenRecorderDelegate, RPPreviewViewControllerDelegate{
 
+    let recorder = RPScreenRecorder.shared()
+    
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startRecording(_ sender: Any) {
+        
+        if recorder.isAvailable == true{
+            
+            recorder.delegate = self
+            recorder.isMicrophoneEnabled = true
+            recorder.startRecording(handler:{(err) in
 
+                
+                if let error = err as NSError?{
+                    if error.code == RPRecordingErrorCode.userDeclined.rawValue{
+                        print("User decline app recording")
+                    }else if error.code == RPRecordingErrorCode.insufficientStorage.rawValue{
+                        print("Not enough space in the storage")
+                    }else{
+                        print("A error appears")
+                    }
+                    return
+                }
+                
+                
+            })
+            
+            print("Started recording")
+            self.startButton.isEnabled = false
+            self.stopButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        
+        if recorder.isAvailable == true{
+            recorder.stopRecording(handler: { (previewVC, error) in
+                previewVC?.previewControllerDelegate = self
+                self.present(previewVC!, animated: true, completion: nil)
+            })
+            print("parando")
+        }
+    }
+    
+    func previewControllerDidFinish(_ previewController: RPPreviewViewController) {
+        print("Finished the preview")
+        dismiss(animated: true, completion: nil)
+        startButton.isEnabled = true
+        stopButton.isEnabled = false        
+    }
+    
+    func previewController(_ previewController: RPPreviewViewController, didFinishWithActivityTypes activityTypes: Set<String>) {
+        print("Preview finished activities \(activityTypes)")
+    }
+    
+    func screenRecorderDidChangeAvailability(_ screenRecorder: RPScreenRecorder) {
+        print("The avaliability has changed")
+    }
+    
+    func screenRecorder(_ screenRecorder: RPScreenRecorder, didStopRecordingWith previewViewController: RPPreviewViewController?, error: Error?) {
+        print("Screen record finished")
+    }
 }
-
